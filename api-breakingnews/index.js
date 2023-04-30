1e6d6bebe2d43623866e65122885d491e521711b
barfly <oseias.laborda@gmail.com>
2023-04-29 21:35:51 -0400

    refactor: add route soma

--- api-breakingnews/index.js
+++ api-breakingnews/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 
 // rota tem três coisa:
-// 1 Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+// 1: Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
 //   GET - pega uma info
 //   POST - cria uma info
 //   PUT - altera toda a info
 //   PATH - altera uma parte da info
 //   DELETE - apaga uma info
 
-// 2 name - Um identificador da rota
+// 2: name - Um identificador da rota
 
-// 3 Function (Callback) - Responsável por executar algum comando
+// 3: Function (Callback) - Responsável por executar algum comando
 
 app.get("/route", (req, res) => {
     const soma = 100 + 1;
