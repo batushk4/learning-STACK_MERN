054239cc6381d19b4e9300afb2086f5dbff0021c
barfly <oseias.laborda@gmail.com>
2023-04-29 21:22:56 -0400

    Rotas em uma API

--- api-breakingnews/index.js
+++ api-breakingnews/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+// rota tem três coisa:
+// 1 Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+//   GET - pega uma info
+//   POST - cria uma info
+//   PUT - altera toda a info
+//   PATH - altera uma parte da info
+//   DELETE - apaga uma info
+
+// 2 name - Um identificador da rota
+
+// 3 Function (Callback) - Responsável por executar algum comando
+
+app.get("/route", (req, res) => {
+    const soma = 100 + 1;
+
+    // res.json(soma);
+    res.send({ soma: soma });
 });
 
 app.listen(3000);
