4442020f4b5c9ea16c809c749a7b372af54cc84a
barfly <oseias.laborda@gmail.com>
2023-04-29 22:16:12 -0400

    refactor: add the software archieture

--- api-breakingnews/index.js
+++ api-breakingnews/index.js
@@ -1,24 +1,8 @@
 const express = require("express");
+const userRoute = require("./src/routes/user.route");
 const app = express();
 
-// rota tem três coisa:
-// 1: Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
-//   GET - pega uma info
-//   POST - cria uma info
-//   PUT - altera toda a info
-//   PATH - altera uma parte da info
-//   DELETE - apaga uma info
-
-// 2: name - Um identificador da rota
-
-// 3: Function (Callback) - Responsável por executar algum comando
-
-app.get("/route", (req, res) => {
-    const soma = 100 + 1;
-
-    // res.json(soma);
-    res.send({ soma: soma });
-});
+app.use("/soma", userRoute);
 
 app.listen(3000);
 
